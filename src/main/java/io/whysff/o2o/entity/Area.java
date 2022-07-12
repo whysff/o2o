0cd13d699480df718e059da2ef8777c460d0e39e
whysff <15670151791@126.com>
2022-07-12 09:32:45 +0800

    Area

---
+++ src/main/java/io/whysff/o2o/entity/Area.java
@@ -0,0 +1,15 @@
+package io.whysff.o2o.entity;
+
+import java.util.Date;
+
+/**
+ * @author lxstart  Email:liuxuan1021@126.com
+ * @create 2022/07/12
+ */
+public class Area {
+    private Integer areaId;
+    private String areaName;
+    private Integer priority;
+    private Date createTime;
+    private Date lastEditTime;
+}
