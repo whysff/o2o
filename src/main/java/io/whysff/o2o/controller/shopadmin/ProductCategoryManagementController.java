c01d83409ffc1c306e86bd1ff7b479c65bd44b21
whysff <15670151791@126.com>
2022-07-15 20:30:59 +0800

    商品类别删除Controller层开发

--- src/main/java/io/whysff/o2o/controller/shopadmin/ProductCategoryManagementController.java
+++ src/main/java/io/whysff/o2o/controller/shopadmin/ProductCategoryManagementController.java
@@ -70,4 +70,29 @@ public class ProductCategoryManagementController {
         }
         return modelMap;
     }
+
+    @PostMapping("/removeproductcategory")
+    @ResponseBody
+    private Map<String, Object> removeProductCategory(Long productCategoryId,HttpServletRequest request) {
+        Map<String, Object> modelMap = new HashMap<>();
+        if (productCategoryId != null && productCategoryId > 0) {
+            try {
+                Shop currentShop = (Shop) request.getSession().getAttribute("currentShop");
+                ProductCategoryExection pe = productCategoryService.deleteProductCategory(productCategoryId, currentShop.getShopId());
+                if (pe.getState() == ProductCategoryStateEnum.SUCCESS.getState()) {
+                    modelMap.put("success", true);
+                } else {
+                    modelMap.put("success", false);
+                    modelMap.put("errMsg", pe.getStateInfo());
+                }
+            } catch (ProductCategoryOperationException e) {
+                modelMap.put("success", false);
+                modelMap.put("errMsg", e.toString());
+            }
+        } else {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "请至少选择一个商品类别");
+        }
+        return modelMap;
+    }
 }
