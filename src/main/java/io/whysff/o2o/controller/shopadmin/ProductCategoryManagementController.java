8fdbbd7ef46c27752b49c570c2cff7f71adb6da2
whysff <15670151791@126.com>
2022-07-15 19:53:22 +0800

    商品类别批量添加Controller层开发

--- src/main/java/io/whysff/o2o/controller/shopadmin/ProductCategoryManagementController.java
+++ src/main/java/io/whysff/o2o/controller/shopadmin/ProductCategoryManagementController.java
@@ -1,16 +1,16 @@
 package io.whysff.o2o.controller.shopadmin;
 
 import io.whysff.o2o.dao.ProductCategoryDao;
+import io.whysff.o2o.dto.ProductCategoryExection;
 import io.whysff.o2o.dto.Result;
 import io.whysff.o2o.entity.ProductCategory;
 import io.whysff.o2o.entity.Shop;
 import io.whysff.o2o.enums.ProductCategoryStateEnum;
+import io.whysff.o2o.exceptions.ProductCategoryOperationException;
 import io.whysff.o2o.service.ProductCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -40,5 +40,34 @@ public class ProductCategoryManagementController {
             ProductCategoryStateEnum ps = ProductCategoryStateEnum.INNER_ERROR;
             return new Result<List<ProductCategory>>(false, ps.getState(), ps.getStateInfo());
         }
+    }
+
+    @PostMapping("/addproductcategorys")
+    @ResponseBody
+    private Map<String, Object> addProductCategorys(@RequestBody List<ProductCategory> productCategoryList,HttpServletRequest request) {
+        Map<String, Object> modelMap = new HashMap<>();
+        Shop currentShop = (Shop) request.getSession().getAttribute("currentShop");
+        for (ProductCategory pc : productCategoryList) {
+            pc.setShopId(currentShop.getShopId());
+        }
+        if (productCategoryList != null && productCategoryList.size() > 0) {
+            try {
+                ProductCategoryExection pe = productCategoryService.batchAddProductCategory(productCategoryList);
+                if (pe.getState() == ProductCategoryStateEnum.SUCCESS.getState()) {
+                    modelMap.put("success", true);
+                } else {
+                    modelMap.put("success", false);
+                    modelMap.put("errMsg", pe.getStateInfo());
+                }
+            } catch (ProductCategoryOperationException e) {
+                modelMap.put("success", false);
+                modelMap.put("errMsg", e.toString());
+                return modelMap;
+            }
+        } else {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "请至少输入一个商品类别");
+        }
+        return modelMap;
     }
 }
