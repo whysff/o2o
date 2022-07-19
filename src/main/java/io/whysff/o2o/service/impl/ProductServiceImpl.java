cf103b0c709b53c13f2138bfdb7c4d3c15f2d1c1
whysff <15670151791@126.com>
2022-07-20 01:19:35 +0800

    商品编辑之Service层开发

--- src/main/java/io/whysff/o2o/service/impl/ProductServiceImpl.java
+++ src/main/java/io/whysff/o2o/service/impl/ProductServiceImpl.java
@@ -85,20 +85,19 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     @Transactional
-    /**
-     * 1.若缩略图参数有值，则处理缩略图
-     * 若原先存在缩略图则先删除再添加新图，之后获取缩略图相对路径并赋值给product
-     * 2.若商品详情图列表参数优质，对商品详情图列表进行同样的操作
-     * 3.将tb_product_img下面的该商品原来的商品详情图记录全部清除
-     * 4.更新tb_product_img 和 tb_product的信息
-     */
-    public ProductExecution modifyProduct(Product product, ImageHolder thumbnail, List<ImageHolder> productImgHolderList) throws ProductOperationException {
+    // 1.若缩略图参数有值，则处理缩略图，
+    // 若原先存在缩略图则先删除再添加新图，之后获取缩略图相对路径并赋值给product
+    // 2.若商品详情图列表参数有值，对商品详情图片列表进行同样的操作
+    // 3.将tb_product_img下面的该商品原先的商品详情图记录全部清除
+    // 4.更新tb_product_img以及tb_product的信息
+    public ProductExecution modifyProduct(Product product, ImageHolder thumbnail,
+                                          List<ImageHolder> productImgHolderList) throws ProductOperationException {
         // 空值判断
         if (product != null && product.getShop() != null && product.getShop().getShopId() != null) {
-            // 商品默认信息赋值
+            // 给商品设置上默认属性
             product.setLastEditTime(new Date());
-            // 若商品缩略图不为空且原有缩略图不为空，则删除原有缩略图并添加
-            if (thumbnail!= null) {
+            // 若商品缩略图不为空且原有缩略图不为空则删除原有缩略图并添加
+            if (thumbnail != null) {
                 // 先获取一遍原有信息，因为原来的信息里有原图片地址
                 Product tempProduct = productDao.queryProductById(product.getProductId());
                 if (tempProduct.getImgAddr() != null) {
@@ -115,11 +114,11 @@ public class ProductServiceImpl implements ProductService {
                 // 更新商品信息
                 int effectedNum = productDao.updateProduct(product);
                 if (effectedNum <= 0) {
-                    throw  new ProductOperationException("更新商品信息失败");
+                    throw new ProductOperationException("更新商品信息失败");
                 }
                 return new ProductExecution(ProductStateEnum.SUCCESS, product);
             } catch (Exception e) {
-                throw new ProductOperationException("更新商品信息失败：" + e.toString());
+                throw new ProductOperationException("更新商品信息失败:" + e.toString());
             }
         } else {
             return new ProductExecution(ProductStateEnum.EMPTY);
