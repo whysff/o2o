1ec8eb734e985823cd700d0a7485eb526d829140
whysff <15670151791@126.com>
2022-07-20 01:43:19 +0800

    商品列表展示之Dao层开发和测试

--- src/main/java/io/whysff/o2o/dao/ProductDao.java
+++ src/main/java/io/whysff/o2o/dao/ProductDao.java
@@ -1,12 +1,36 @@
 package io.whysff.o2o.dao;
 
 import io.whysff.o2o.entity.Product;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * @author lxstart  Email:liuxuan1021@126.com
  * @create 2022/07/17
  */
 public interface ProductDao {
+
+    /**
+     * 查询商品列表并分页，可输入的条件有：商品名（模糊），商品状态，店铺id，商品类别
+     *
+     * @param productCondition
+     * @param rowIndex
+     * @param pageSize
+     * @return
+     */
+    List<Product> queryProductList(@Param("productCondition")  Product productCondition,
+                                   @Param("rowIndex") int rowIndex,
+                                   @Param("pageSize") int pageSize);
+
+    /**
+     * 查询对应的商品总数
+     *
+     * @param productCondition
+     * @return
+     */
+    int queryProductCount(@Param("productCondition") Product productCondition);
+
 
     /**
      * 通过productId查询唯一的商品信息
--- src/test/java/io/whysff/o2o/dao/ProductDaoTest.java
+++ src/test/java/io/whysff/o2o/dao/ProductDaoTest.java
@@ -30,27 +30,67 @@ public class ProductDaoTest extends BaseTest {
     private ProductImgDao productImgDao;
 
     @Test
-    public void testAInsertProduct() {
+    public void testAInsertProduct() throws Exception {
+        Shop shop1 = new Shop();
+        shop1.setShopId(1L);
+        ProductCategory pc1 = new ProductCategory();
+        pc1.setProductCategoryId(1L);
+        // 初始化三个商品实例并添加进shopId为1的店铺里，
+        // 同时商品类别Id也为1
+        Product product1 = new Product();
+        product1.setProductName("测试1");
+        product1.setProductDesc("测试Desc1");
+        product1.setImgAddr("test1");
+        product1.setPriority(1);
+        product1.setEnableStatus(1);
+        product1.setCreateTime(new Date());
+        product1.setLastEditTime(new Date());
+        product1.setShop(shop1);
+        product1.setProductCategory(pc1);
+        Product product2 = new Product();
+        product2.setProductName("测试2");
+        product2.setProductDesc("测试Desc2");
+        product2.setImgAddr("test2");
+        product2.setPriority(2);
+        product2.setEnableStatus(0);
+        product2.setCreateTime(new Date());
+        product2.setLastEditTime(new Date());
+        product2.setShop(shop1);
+        product2.setProductCategory(pc1);
+        Product product3 = new Product();
+        product3.setProductName("test3");
+        product3.setProductDesc("测试Desc3");
+        product3.setImgAddr("test3");
+        product3.setPriority(3);
+        product3.setEnableStatus(1);
+        product3.setCreateTime(new Date());
+        product3.setLastEditTime(new Date());
+        product3.setShop(shop1);
+        product3.setProductCategory(pc1);
+        // 判断添加是否成功
+        int effectedNum = productDao.insertProduct(product1);
+        assertEquals(1, effectedNum);
+        effectedNum = productDao.insertProduct(product2);
+        assertEquals(1, effectedNum);
+        effectedNum = productDao.insertProduct(product3);
+        assertEquals(1, effectedNum);
+    }
 
-        Shop shop = new Shop();
-        shop.setShopId(1L);
-        ProductCategory pc = new ProductCategory();
-        pc.setProductCategoryId(1L);
-
-        Product product = new Product();
-        product.setProductName("测试商品");
-        product.setProductDesc("测试描述");
-        product.setImgAddr("测试地址");
-        product.setPriority(1);
-        product.setCreateTime(new Date());
-        product.setLastEditTime(new Date());
-        product.setEnableStatus(1);
-        product.setShop(shop);
-        product.setProductCategory(pc);
-
-        int effectedNum = productDao.insertProduct(product);
-        assertEquals(1,effectedNum);
-
+    @Test
+    public void testBQueryProductList() throws Exception {
+        Product productCondition = new Product();
+        // 分页查询，预期返回三条结果
+        List<Product> productList = productDao.queryProductList(productCondition, 0, 3);
+        assertEquals(3, productList.size());
+        // 查询名称为测试的商品总数
+        int count = productDao.queryProductCount(productCondition);
+        assertEquals(5, count);
+        // 使用商品名称模糊查询，预期返回两条结果
+        productCondition.setProductName("测试");
+        productList = productDao.queryProductList(productCondition, 0, 3);
+        assertEquals(3, productList.size());
+        count = productDao.queryProductCount(productCondition);
+        assertEquals(3, count);
     }
 
     @Test
