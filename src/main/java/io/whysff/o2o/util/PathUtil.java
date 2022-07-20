3dd482b18dd562a9540eddd44f791b36a492a75a
whysff <15670151791@126.com>
2022-07-20 13:51:05 +0800

    修改保存图片的路径信息

--- src/main/java/io/whysff/o2o/util/PathUtil.java
+++ src/main/java/io/whysff/o2o/util/PathUtil.java
@@ -14,14 +14,14 @@ public class PathUtil {
         if(os.toLowerCase().startsWith("win")) {
             basePath = "D:/projectdev/image";
         } else {
-            basePath = "/home/lx/image/";
+            basePath = "/Users/baidu/work/image";
         }
         basePath = basePath.replace("/", seperator);
         return basePath;
     }
 
     public static String getShopImagePath(Long shopId) {
-        String imagePath = "/upload/item/shop/" + shopId +"/";
+        String imagePath = "/upload/images/item/shop/" + shopId +"/";
         return imagePath.replace("/",seperator);
     }
 }
