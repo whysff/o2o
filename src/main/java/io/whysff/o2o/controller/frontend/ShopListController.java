c325adcf556f7033bd1f44b36a99fba9a3a64695
whysff <15670151791@126.com>
2022-07-20 16:28:37 +0800

    店铺列表Controller层开发与测试,Dao层mapper文件修改：s.enableStatus该为：s.enable_status

--- src/main/java/io/whysff/o2o/controller/frontend/ShopListController.java
+++ src/main/java/io/whysff/o2o/controller/frontend/ShopListController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -90,28 +91,28 @@ public class ShopListController {
      * @param request
      * @return
      */
-    @GetMapping("/listshops")
+    @RequestMapping(value = "/listshops", method = RequestMethod.GET)
     @ResponseBody
     private Map<String, Object> listShops(HttpServletRequest request) {
-        Map<String, Object> modelMap = new HashMap<>();
+        Map<String, Object> modelMap = new HashMap<String, Object>();
         // 获取页码
         int pageIndex = HttpServletRequestUtil.getInt(request, "pageIndex");
         // 获取一页需要显示的数据条数
         int pageSize = HttpServletRequestUtil.getInt(request, "pageSize");
-        //  非空判断
-        if (pageIndex > -1 && pageSize > -1) {
-            // 试着获取一级类别id
+        // 非空判断
+        if ((pageIndex > -1) && (pageSize > -1)) {
+            // 试着获取一级类别Id
             long parentId = HttpServletRequestUtil.getLong(request, "parentId");
-            // 试着获取特定二级类别id
+            // 试着获取特定二级类别Id
             long shopCategoryId = HttpServletRequestUtil.getLong(request, "shopCategoryId");
-            // 试着获取区域id
+            // 试着获取区域Id
             int areaId = HttpServletRequestUtil.getInt(request, "areaId");
             // 试着获取模糊查询的名字
             String shopName = HttpServletRequestUtil.getString(request, "shopName");
             // 获取组合之后的查询条件
             Shop shopCondition = compactShopCondition4Search(parentId, shopCategoryId, areaId, shopName);
             // 根据查询条件和分页信息获取店铺列表，并返回总数
-            ShopExecution se = shopService.getShopList(shopCondition,pageIndex,pageSize);
+            ShopExecution se = shopService.getShopList(shopCondition, pageIndex, pageSize);
             modelMap.put("shopList", se.getShopList());
             modelMap.put("count", se.getCount());
             modelMap.put("success", true);
@@ -119,6 +120,7 @@ public class ShopListController {
             modelMap.put("success", false);
             modelMap.put("errMsg", "empty pageSize or pageIndex");
         }
+
         return modelMap;
     }
 
